a3097c1e3bcd6fd136bdd53514b626bad115e5cf
diegomucientes <80188066+diegomucientes@users.noreply.github.com>
2021-04-13 20:36:13 -0400

    ConvertToFunctional

--- src/Components/PersonCard.js
+++ src/Components/PersonCard.js
@@ -1,50 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './PersonCard.css'; 
 
 
-class PersonCard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            ageperson: this.props.age 
-        };
-            
-    }
-    upFunction = () => this.setState({
-        ageperson: this.state.ageperson +1
+
+const PersonCard = props => {    
+    const [state, setState] = useState({
+        ageperson: props.age
+    });
+    
+    
+    const upFunction = () => setState({
+        ageperson: state.ageperson +1
     })
         
-    downFunction = () => this.setState({
-        ageperson: this.state.ageperson -1 
+    const downFunction = () => setState({
+        ageperson: state.ageperson -1 
     })
+    return(        
+        <div>            
+            <h1>{props.lastName}, {props.firstName}</h1>            
+            <p>Edad: {state.ageperson}</p>   
+            <button onClick= {upFunction}>Subele</button>   
+            <button onClick={downFunction}>Bajale</button>  
+            <p>Hair Color: {props.hairColor}</p>        
+        </div>    
         
-    
-    render(){
-        const { firstName, lastName,age, hairColor} = this.props;
-        
-    
-
-        return(
-            <div>
-                <h1>
-                    {firstName},{lastName}
-                </h1>
-                <p>
-                    age: {this.state.ageperson}
-                </p>
-                 <p>
-                    hair color: {hairColor}
-                 </p>
-                 
-                 <button onClick = {this.upFunction}>
-                     Click age Up
-                 </button>
-                 <button onClick = {this.downFunction}>
-                     Click age Down
-                 </button>
-            </div>
-        )
-    }
-
+    );
 }
+
+
+
 export default PersonCard;
+    
+
+// class PersonCard extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             ageperson: this.props.age 
+//         };
+            
+//     }
+    
+//     render(){
+//         const { firstName, lastName,age, hairColor} = this.props;
+        
+    
+
+//         return(
+//             <div>
+//                 <h1>
+//                     {firstName},{lastName}
+//                 </h1>
+//                 <p>
+//                     age: {this.state.ageperson}
+//                 </p>
+//                  <p>
+//                     hair color: {hairColor}
+//                  </p>
+                 
+//                  <button onClick = {this.upFunction}>
+//                      Click age Up
+//                  </button>
+//                  <button onClick = {this.downFunction}>
+//                      Click age Down
+//                  </button>
+//             </div>
+//         )
+//     }
+
+// }
+// export default PersonCard;
