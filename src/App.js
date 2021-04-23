96d1028974332c3a6f69a25bbed7620018558561
diegomucientes <80188066+diegomucientes@users.noreply.github.com>
2021-04-22 20:45:49 -0400

    ejercicio apoyalo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <PersonCard firstName = "Jane" lastName = "Doe" age = {45} hairColor ="clack"/>
       <PersonCard firstName = "John" lastName = "Smith" age = {80} hairColor ="brown"/>
       <PersonCard firstName = "Fillmore" lastName = "Millard" age = {50} hairColor ="brown"/>
-      <PersonCard firstName = "Maria " lastName = "Smith" age = {62} hairColor ="brown"/>
+      <PersonCard firstName = "Marigit " lastName = "Smith" age = {62} hairColor ="brown"/>
     </div>
   );
 }
